960b3f5b281056a9b50e5eca0d329db3025b6a02
BrunoMarques75 <220001110@esg.ipsantarem.pt>
2023-11-09 21:21:00 +0000

    update

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,46 +1,43 @@
 package com.example.weatherapp
 
-import android.graphics.Rect
 import android.os.Bundle
-import android.util.LayoutDirection
-import android.util.Size
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Outline
-import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import com.example.weatherapp.ui.theme.WeatherAppTheme
-
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,89 +49,145 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MenuPrincipal()
+                    Greeting("Android")
                 }
             }
         }
     }
 }
 
-@Preview(showBackground = true, widthDp = 390, heightDp = 800)
 @Composable
-fun MenuPrincipal(): Unit {
-    Column() {
-        NavegacaoPesquisaComponente()
-        MensagemBemVindoComponente()
-        ImagemPreviewComponente()
-        PrevisaoDias()
-    }
-
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
 }
 
-@Preview(showBackground = true, widthDp = 380)
+@Preview(showBackground = true, heightDp = 800, widthDp = 380)
 @Composable
-fun NavegacaoPesquisaComponente(): Unit {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(15.dp),
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically
-    )
-    {
-        Button(
-            onClick = { /*TODO*/ },
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color.Transparent,
-                contentColor = Color.White
-            )
-        )
-            {
-                val rainbowColorsBrush = remember {
-                    Brush.sweepGradient(
-                        listOf(
-                            Color(0xFF9575CD),
-                            Color(0xFFBA68C8),
-                            Color(0xFFE57373),
-                            Color(0xFFFFB74D),
-                            Color(0xFFFFF176),
-                            Color(0xFFAED581),
-                            Color(0xFF4DD0E1),
-                            Color(0xFF9575CD)
-                        )
-                    )
-                }
-                val borderWidth = 4.dp
+fun page(): Unit {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(12.dp))  {
+        Cabecalho()
+        dataNome("Bruno")
+        imagemCentral()
+    }
+}
+
+@Preview(showBackground = true, heightDp = 300, widthDp = 480)
+@Composable
+fun imagemCentral(): Unit {
+    Box(modifier = Modifier
+        .height(300.dp)
+        .background(Color.Transparent)
+        .fillMaxSize()
+    ) {
+        Column {
+            Box(
+                modifier = Modifier
+                .fillMaxSize()
+                .padding(15.dp),
+                contentAlignment = Alignment.TopCenter
+            ) {
                 Image(
-                    painter = painterResource(id = R.drawable.cr7),
-                    contentDescription = stringResource(id = R.string.lupa_content_description),
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .size(100.dp)
-                        .border(
-                            BorderStroke(borderWidth, rainbowColorsBrush),
-                        )
+                    painter = painterResource(id = R.drawable.sun),
+                    contentDescription = "sol"
+                )
+            }
+            Row {
+                Text(text = "12ºC",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = TextUnit(15f, TextUnitType.Sp),
+                    color = MaterialTheme.colorScheme.tertiary
+                )
+                Text(text = "Parcialmente nublado",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = TextUnit(15f, TextUnitType.Sp),
+                    color = MaterialTheme.colorScheme.tertiary
                 )
             }
         }
     }
 
-
-@Preview(showBackground = true, widthDp = 380)
-@Composable
-fun MensagemBemVindoComponente(): Unit {
-    Text(text = "Bem-Vindo");
 }
 
+
+
+@Preview(showBackground = true, heightDp = 50, widthDp = 380)
+@Composable
+fun Cabecalho(): Unit {
+    Row (
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(8.dp,8.dp)
+    ){
+
+        Button(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+            contentPadding = PaddingValues(),
+            modifier = Modifier.size(35.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.search),
+                contentDescription = R.string.Menu_search.toString()
+            )
+        }
+        Button(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+            contentPadding = PaddingValues(),
+            modifier = Modifier
+                .clip(CircleShape)
+                .width(35.dp)
+        ) {
+
+                Image(
+                    painter = painterResource(id = R.drawable.foto),
+                    contentDescription = R.string.Menu_foto.toString())
+            }
+        }
+    }
+
 @Preview(showBackground = true, widthDp = 380)
 @Composable
-fun ImagemPreviewComponente(): Unit {
-    Text(text = "Imagem");
+fun dataNome (name: String = "Bruno"): Unit {
+
+    Column (modifier = Modifier.fillMaxSize()) {
+        Row(modifier = Modifier.fillMaxSize()) {
+            Text(
+                text = "Olá",
+                modifier = Modifier.padding(end = 2.dp),
+                fontSize = TextUnit(25f, TextUnitType.Sp),
+                color = MaterialTheme.colorScheme.primary
+            )
+            Text(
+                text = "$name",
+                fontWeight = FontWeight.Bold,
+                fontSize = TextUnit(25f, TextUnitType.Sp),
+                color = MaterialTheme.colorScheme.primary
+            )
+
+            Text(
+                text = formatData(Calendar.getInstance().time),
+                fontWeight = FontWeight.Light,
+                fontSize = TextUnit(18f, TextUnitType.Sp),
+                color = MaterialTheme.colorScheme.secondary
+            )
+            Text(
+                text = "Alenquer",
+                fontWeight = FontWeight.Light,
+                fontSize = TextUnit(18f, TextUnitType.Sp),
+                color = MaterialTheme.colorScheme.secondary
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true, widthDp = 380)
-@Composable
-fun PrevisaoDias(): Unit {
-    Text(text = "Previsão");
 
+fun formatData(data: Date): String {
+    val format = SimpleDateFormat("dd MMMM, EEEE yyyy", Locale("pt", "PT"))
+    return format.format(data)
 }
